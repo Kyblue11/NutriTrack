68db1521d7d1db523ed0f299f7ccc717080abaec
aaron <alam0055@student.monash.edu>
2025-05-08 14:37:58 +0800

    fix typo bug

--- app/src/main/java/com/aaronlamkongyew33521808/myapplication/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/aaronlamkongyew33521808/myapplication/ui/register/RegisterActivity.kt
@@ -11,23 +11,19 @@ import com.aaronlamkongyew33521808.myapplication.ui.login.LoginActivity
 class RegisterActivity : ComponentActivity() {
     private val vm by viewModels<RegisterViewModel>()
 
-    class RegisterActivity : ComponentActivity() {
-        private val vm by viewModels<RegisterViewModel>()
-
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContent {
-                RegisterScreen(
-                    viewModel = vm,
-                    onRegisterSuccess = { userId ->
-                        // after registration go back to Login
-                        val intent = Intent(this, LoginActivity::class.java)
-                        intent.putExtra("userId", userId)
-                        startActivity(intent)
-                        finish()
-                    }
-                )
-            }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RegisterScreen(
+                viewModel = vm,
+                onRegisterSuccess = { userId ->
+                    // after registration go back to Login
+                    val intent = Intent(this, LoginActivity::class.java)
+                    intent.putExtra("userId", userId)
+                    startActivity(intent)
+                    finish()
+                }
+            )
         }
     }
 }
