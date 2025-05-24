989ab0a616e97716493f64eb1d40320c12eee35e
alam0055 <alam0055@student.monash.edu>
2025-05-24 20:30:19 +0800

    comments for getAll

--- app/src/main/java/com/aaronlamkongyew33521808/myapplication/data/api/FruityViceApi.kt
+++ app/src/main/java/com/aaronlamkongyew33521808/myapplication/data/api/FruityViceApi.kt
@@ -6,7 +6,10 @@ interface FruityViceApi {
     @GET("api/fruit/all")
     suspend fun getAllFruits(): List<Fruit>
 
-// Is getting all, then filtering to find a specific fruit bad?
+// Is getting all, then filtering to find a specific fruit not optimal?
 // Should i have a singular get fruit function?
 // But, what if a user spams get requests many times? Then one getAll request is more efficient!
+// Since the total number of fruits is small, it is okay to get all fruits at once!
+
+// Also, the `/all` call solves the problem of the space bar " " API problem for fruits like "horned melon"
 }
